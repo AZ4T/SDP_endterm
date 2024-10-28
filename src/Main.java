4fd73bfe2dc602dad632a743288f6f340872aa56
Biba <bibushka2505@icloud.com>
2024-10-28 21:19:53 +0500

    fourth commit test by Biba

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(321); // biba
+        System.out.println("Hello Azat"); //biba
     }
 }
