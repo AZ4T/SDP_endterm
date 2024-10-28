d54686e518000c1b327e3ad936622a136788b4fb
Asus <sultan.kissamedinov@gmail.com>
2024-10-28 21:30:20 +0500

    fifth commit test by Sultan

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println(123);
         System.out.println(321); // biba
         System.out.println("Hello Azat"); //biba
+        System.out.println("2"); //sultan
     }
 }
